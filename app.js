c08df48b7a91e455e10f65affc231bd4a9accfba
d0dders <dodders@outlook.com>
2020-05-04 23:15:53 +0100

    Refactored playing sound to a function

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-window.addEventListener('keydown', (e) => {
+window.addEventListener('keydown', playSound);
+
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; //stop running
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-});
+}
 
 function removeTransition(e) {
     if(e.propertyName !== 'transform') return; // only remove transition after transform
@@ -13,6 +15,6 @@ function removeTransition(e) {
 }
 
 const keys = document.querySelectorAll('.key');
-    keys.forEach(key => {
+keys.forEach(key => {
     key.addEventListener('transitionend', removeTransition)
 });
